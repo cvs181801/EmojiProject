16a3d2cc6ce3135f6d6fa362b402123aa05b1cd0
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-02 19:31:23 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//grab the emoji container element
+const emojisContained = document.getElementById("emojiContainer");
+console.log(emojisContained)
+
+//add your emojis in an array
+const myEmojis = ["A", "B", "C"]
+//loop through emojis array
+for (let i = 0; i < myEmojis.length; i++) {
+    var renderEmojis = document.createElement("span")
+    renderEmojis.textContent = myEmojis[i]
+    emojiContainer.append(renderEmojis)
+
+    const pushEmojiButton = document.getElementById("push-btn")
+
+pushEmojiButton.addEventListener("click", function () {
+    const emojiInput = document.getElementById("emoji-input")
+    console.log(emojiInput)
+})
