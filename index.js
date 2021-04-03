ae850a77a9d526df95b4cf61cc6754a326403aad
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-02 19:59:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,23 @@ const emojisContained = document.getElementById("emojiContainer");
 //add your emojis in an array
 const myEmojis = ["⏳", "🖤", "💀"]
 // //loop through emojis array
-for (let i = 0; i < myEmojis.length; i++) {
-     var renderEmojis = document.createElement("span")
-     renderEmojis.textContent = myEmojis[i]
-     emojiContainer.append(renderEmojis)
-
-    const pushEmojiButton = document.getElementById("push-btn")
-
- pushEmojiButton.addEventListener("click", function () {
-     const emojiInput = document.getElementById("emoji-input")
-     if (emojiInput.value) {
-        myEmojis.push(emojiInput.value)
-        emojiInput.value = ""
-        console.log(myEmojis)
+function renderEmojis() {
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+       }
+    }
+    
+    renderEmojis()
+    
+    const pushBtn = document.getElementById("push-btn")
+    pushBtn.addEventListener("click", function(){
+        const emojiInput = document.getElementById("emoji-input")
+        if (emojiInput.value) {
+            myEmojis.push(emojiInput.value)
+            emojiInput.value = "" 
+            emojiContainer.innerHTML = ""
+            renderEmojis()
         }
     })
